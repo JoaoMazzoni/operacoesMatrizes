a441e59e27d79097082efb999af50129e6f55c72
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-25 12:55:14 -0300

    Ajustes em impressões

--- operacoesMatrizes/Program.cs
+++ operacoesMatrizes/Program.cs
@@ -2,17 +2,18 @@
 {
     int operacao = 0;
     int qtdlinhas = 0, qtdcolunas = 0;
+    bool TrueFalse = true;
+
+
     void tamanhoMatriz()
     {
         Console.Write("Digite o número de linhas: ");
         qtdlinhas = int.Parse(Console.ReadLine());
         Console.Write("\nDigite o número de colunas: ");
         qtdcolunas = int.Parse(Console.ReadLine());
-        
     }
 
     tamanhoMatriz();
-
     float[,] matriz1 = new float[qtdlinhas, qtdcolunas];
     float[,] matriz2 = new float[qtdlinhas, qtdcolunas];
     float[,] matriz3 = new float[qtdlinhas, qtdcolunas];
@@ -29,6 +30,7 @@
         }
         imprimirMatriz(matriz, "\n\nMatriz Gerada\n");
     }
+
     void imprimirMatriz(float[,] matriz, string titulo)
     {
         Console.WriteLine(titulo);
@@ -44,6 +46,7 @@
 
         }
     }
+
     void somarMatriz(float[,] matriz1, float[,] matriz2)
     {
         for (int linha = 0; linha < qtdlinhas; linha++)
@@ -110,28 +113,30 @@
 
     sortearMatriz(matriz1);
     sortearMatriz(matriz2);
+
     operacao = menuSelecao();
     switch (operacao)
     {
         case 1:
             somarMatriz(matriz1, matriz2);
-            break;
+        break;
 
         case 2:
             subtrairMatriz(matriz1, matriz2);
-            break;
+        break;
 
         case 3:
             multiplicarMatriz(matriz1, matriz2);
-            break;
+        break;
 
         case 4:
             dividirMatriz(matriz1, matriz2);
-            break;
+        break;
 
         default:
             Console.WriteLine("\n\n|Opção inválida|\n");
-            break;
+            TrueFalse = false;   
+        break;
     }
 
     Console.ReadKey();
